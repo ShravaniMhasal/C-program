00f289f1a85e1e32aa74a847786dcf95c2d08f00
ShravaniMhasal <shravanimnextgen@gmail.com>
2026-01-13 13:17:42 +0530

    Fix formatting and whitespace handling in palindrome check

--- palindromearray.c
+++ palindromearray.c
@@ -1,29 +1,37 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+
 int main()
 {
-    char str[100]="Madam";
-    int left =0;
-    int right = strlen(str)-1;
-    while(left<right)
+    char str[100] = "Madam";
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while(left < right)
     {
-        if (str[left]==' ')
+        if(str[left] == ' ')
         {
             left++;
             continue;
         }
-        if (str[right]!=' ')
+
+        if(str[right] == ' ')
         {
             right--;
             continue;
         }
-        if(tolower(str[left])!=tolower(str[right]))
+
+        if(tolower(str[left]) != tolower(str[right]))
         {
             printf("Not a palindrome");
+            return 0;
         }
+
         left++;
         right--;
     }
+
+    printf("Palindrome");
     return 0;
 }
